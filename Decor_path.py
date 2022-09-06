a800e6e032fb5e6734fca400ab0e9b0acec4f4e3
Budarin Sergey <budarin_s@mail.ru>
2022-09-06 09:52:08 +0300

    Commit Decor_path

--- Decor_path.py
+++ Decor_path.py
@@ -1,29 +1,34 @@
+
 from datetime import datetime
+import requests
+
 
 FILE_PATH = 'decoratorlogs.txt'
 
-def log_func (logs_file_path):
-    with open(logs_file_path, 'w', encoding="utf-8") as file_obj:
-        result1 = f'{datetime.now()}  n'
-        file_obj.write(result1)
+
+def parametrized_decor(path):
+    def decor(func):
+        def new_foo(*args, **kwargs):
+            date_time = datetime.now()
+            func_name = func.__name__
+            result = func(*args, **kwargs)
+            with open(path, 'w', encoding='utf-8') as file:
+                file.write(f'Дата/время: {date_time}\n'
+                           f'Имя функции: {func_name}\n'
+                           f'Аргументы: {args, kwargs}\n'
+                           f'Результат: {result}\n')
+            return result
+        return new_foo
+    return decor
 
 
-def cache(old_fun):
-
-  def new_fun(*args, **kwargs):
-    print(f'вызвана функция: {old_fun} с аргументами {args} и {kwargs}. Время вызова функции: {datetime.now()}')
-
-    result = old_fun(*args, **kwargs)
-    log_func(FILE_PATH)
-    print(f'Результат {result}')
-    return result
+@parametrized_decor(FILE_PATH)
+def get_status(*args, **kwargs):
+    url = ','.join(args)
+    response = requests.get(url=url)
+    return response.status_code
 
 
-  return new_fun
+if __name__ == '__main__':
+    print(get_status('https://yandex.ru/'))
 
-# @cache
-# def summ(a,b):
-#   return a+b
-#
-#
-# summ(2,6)
